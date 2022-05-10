1af21514f7ddfc6f96f30337b8e927d935fa3118
xkozubi1 <xkozubi1@fi.muni.cz>
2022-05-10 11:26:24 +0200

    add authorization

--- visualize.py
+++ visualize.py
@@ -1,5 +1,23 @@
+import spotipy
+from spotipy import SpotifyOAuth
+
+
 def run():
-    print("Hello world")
+    authorize()
+
+def authorize():
+
+    client_id = ''
+    client_secret = ''
+    redirect_uri = ''
+
+    scope = 'playlist-modify-private user-read-private user-library-read user-top-read playlist-read-private'
+    username = ''
+
+    token = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope,
+                         username=username)
+
+    return spotipy.Spotify(auth_manager=token)
 
 if __name__ == "__main__":
     run()
