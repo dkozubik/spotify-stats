a6a096702584234326e50412f3f0ace3e7bd7f31
xkozubi1 <xkozubi1@fi.muni.cz>
2022-05-11 13:02:55 +0200

    add playlists summary table

--- visualize.py
+++ visualize.py
@@ -2,6 +2,7 @@ import spotipy
 from spotipy import SpotifyOAuth
 import plotly.graph_objects as go
 import plotly.subplots as sub
+from statistics import mean
 
 
 def run():
@@ -10,6 +11,7 @@ def run():
     show_user_top_tracks(spotify, figures)
     show_user_top_artists(spotify, figures)
     show_user_top_genres(spotify, figures)
+    show_user_playlists_summary(spotify, figures)
     show_figures(figures)
 
 
@@ -101,6 +103,42 @@ def show_user_top_genres(spotify, figures):
     figures.append(pie_obj)
 
 
+def show_user_playlists_summary(spotify, figures):
+    playlists = spotify.current_user_playlists()
+    data = []  # (playlist name, tracks count, avg song duration, first song name)
+
+    for playlist in playlists['items']:
+        playlist_tracks = spotify.playlist_tracks(playlist_id=playlist['uri'])
+        name = playlist['name']
+        track_count = len(
+            playlist_tracks['items'])  # Also, could use <playlist['total]> but it doesn't work on blend-type playlists
+        avg_duration_seconds = round(mean([track['track']['duration_ms'] for track in playlist_tracks['items']]) / 1000,
+                                     2)
+        first_track = playlist_tracks['items'][0]['track']['name']
+
+        data.append((name, track_count, avg_duration_seconds, first_track))
+
+    col1 = [item[0] for item in data]
+    col2 = [item[1] for item in data]
+    col3 = [item[2] for item in data]
+    col4 = [item[3] for item in data]
+
+    table_obj = go.Table(
+        header=dict(values=['Playlist name', 'Total tracks', 'Average song duration (s)', 'First song in the playlist'],
+                    line_color='darkslategray',
+                    fill_color='lightskyblue',
+                    align='center'),
+        cells=dict(values=[col1, col2, col3, col4],
+                   line_color='darkslategray',
+                   fill_color='lightcyan',
+                   align='center',
+                   font_size=15,
+                   height=30
+                   ))
+
+    figures.append(table_obj)
+
+
 def show_figures(figures):
     fig = sub.make_subplots(
         rows=2, cols=2,
@@ -113,7 +151,9 @@ def show_figures(figures):
     fig.add_trace(figures[0], row=1, col=1)  # top tracks
     fig.add_trace(figures[1], row=2, col=1)  # top artists
     fig.add_trace(figures[2], row=1, col=2)  # top genres
+    fig.add_trace(figures[3], row=2, col=2)  # playlists summary
 
+    fig.update_layout(width=1200, height=1000)
     fig.show()
 
 
